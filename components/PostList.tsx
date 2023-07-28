492800d1fac8052a51ecb7df140086a1fdbfdd76
samaybar <samaybar@users.noreply.github.com>
2023-07-28 17:16:46 -0400

    Update PostList.tsx

--- components/PostList.tsx
+++ components/PostList.tsx
@@ -5,6 +5,10 @@ type PostListProps = {
 export default function PostList(props: PostListProps) {
   const { files } = props;
   return (
+    <Head>
+<script type="text/javascript">!function(e,t,n){function a(){var e=t.getElementsByTagName("script")[0],n=t.createElement("script");n.type="text/javascript",n.async=!0,n.src="https://beacon-v2.helpscout.net",e.parentNode.insertBefore(n,e)}if(e.Beacon=n=function(t,n,a){e.Beacon.readyQueue.push({method:t,options:n,data:a})},n.readyQueue=[],"complete"===t.readyState)return a();e.attachEvent?e.attachEvent("onload",a):e.addEventListener("load",a,!1)}(window,document,window.Beacon||function(){});
+</script><script type="text/javascript">window.Beacon('init', '1507132f-fad2-434d-ad35-0f7fe289d0d0')</script>
+</Head>
     <ul class="list-disc ml-5 mr-5 group">
       {files.map((file: string) => {
         const postFile = file.split(".");
